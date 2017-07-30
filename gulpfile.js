bde903bf2bbd758811ba2a1a7aa76cc0efb6c483
lukeajtodd <lukeajtodd@outlook.com>
2017-07-30 18:41:20 +0100

    Working stage, needs content and images

--- gulpfile.js
+++ gulpfile.js
@@ -79,8 +79,8 @@ gulp.task('images', () => {
 
 gulp.task('fonts', () => {
   return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
-    .concat('app/fonts/**/*'))
-    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/fonts')));
+    .concat('app/styles/fonts/**/*'))
+    .pipe($.if(dev, gulp.dest('.tmp/styles/fonts'), gulp.dest('dist/styles/fonts')));
 });
 
 gulp.task('extras', () => {
