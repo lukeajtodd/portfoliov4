2397ea266ab279d2093de515928a0fa039c693e1
lukeajtodd <lukeajtodd@outlook.com>
2017-08-10 14:18:28 +0100

    Redo stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+var night_box = document.querySelector('.night');
+var star_limit = 45;
+
+for (var i = 0; i < star_limit; i++) {
+    var star = document.createElement('div');
+    star.classList.add('star');
+    star.style.width = (Math.floor(Math.random() * (5 - 2) + 2)) + 'px';
+    star.style.height = star.style.width;
+
+    star.style.top = Math.floor(Math.random() * 100) + '%';
+    star.style.left = Math.floor(Math.random() * 100) + '%';
+
+    night_box.appendChild(star);
+    
+}
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";for(var night_box=document.querySelector(".night"),star_limit=45,i=0;i<star_limit;i++){var star=document.createElement("div");star.classList.add("star"),star.style.width=Math.floor(3*Math.random()+2)+"px",star.style.height=star.style.width,star.style.top=Math.floor(100*Math.random())+"%",star.style.left=Math.floor(100*Math.random())+"%",night_box.appendChild(star)}
